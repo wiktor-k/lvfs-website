c25718ebea61588e94c2d158f0e8f1ec22605ab0
Richard Hughes <richard@hughsie.com>
2015-10-04 21:13:55 +0100

    Fix up the sync script

--- sync.sh
+++ sync.sh
@@ -1,2 +1,8 @@
+cd ../python-cabarchive
+git pull
+cd -
+cd ../python-appstream
+git pull
+cd -
 cp ../python-cabarchive/cabarchive/*.py cabarchive/
 cp ../python-appstream/appstream/*.py appstream/
