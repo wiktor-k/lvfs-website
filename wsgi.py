373f34ba6ba18d5b79eca070ec6e01a196686cec
Richard Hughes <richard@hughsie.com>
2015-10-08 13:34:33 +0100

    trivial: Make sizeof_fmt() more robust

--- wsgi.py
+++ wsgi.py
@@ -59,6 +59,8 @@ def _qa_hash(value):
 
 def sizeof_fmt(num, suffix='B'):
     """ Generate user-visible size """
+    if not type(num) in (int, long):
+        return "???%s???" % num
     for unit in ['', 'Ki', 'Mi', 'Gi', 'Ti', 'Pi', 'Ei', 'Zi']:
         if abs(num) < 1024.0:
             return "%3.1f%s%s" % (num, unit, suffix)
