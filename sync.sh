4546ae518f7cbff21ecdea3751d0b93ff4879b41
Richard Hughes <richard@hughsie.com>
2017-05-09 13:24:24 +0100

    trivial: Do not use the sync script

--- sync.sh
+++
@@ -1,8 +0,0 @@
-cd ../python-cabarchive
-git pull
-cd -
-cd ../python-appstream
-git pull
-cd -
-cp ../python-cabarchive/cabarchive/*.py cabarchive/
-cp ../python-appstream/appstream/*.py appstream/
