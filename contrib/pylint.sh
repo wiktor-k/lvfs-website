d738c6dce127919bbc9946cdf48fac99ef72dad6
Richard Hughes <richard@hughsie.com>
2017-05-09 13:23:42 +0100

    trivial: Always use python-2 linting

--- contrib/pylint.sh
+++ contrib/pylint.sh
@@ -1,1 +1,1 @@
-pylint $1 --disable=line-too-long,invalid-name,too-many-return-statements,protected-access
+pylint-2 $1 --disable=line-too-long,invalid-name,too-many-return-statements,protected-access
