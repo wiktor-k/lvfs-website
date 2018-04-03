31e6bb71f980dea1ceb8c3df9c4b01b0679d7bdb
Richard Hughes <richard@hughsie.com>
2018-04-03 11:09:24 +0100

    Use actual boolean types in the user table

--- migrations/env.py
+++ migrations/env.py
@@ -27,6 +27,8 @@ target_metadata = current_app.extensions['migrate'].db.metadata
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
 
+# set to True to detect type changes in a better way (with false positives)
+COMPARE_TYPES = False
 
 def run_migrations_offline():
     """Run migrations in 'offline' mode.
@@ -41,7 +43,8 @@ def run_migrations_offline():
 
     """
     url = config.get_main_option("sqlalchemy.url")
-    context.configure(url=url)
+    context.configure(compare_type=COMPARE_TYPES,
+                      url=url)
 
     with context.begin_transaction():
         context.run_migrations()
@@ -71,6 +74,7 @@ def run_migrations_online():
 
     connection = engine.connect()
     context.configure(connection=connection,
+                      compare_type=COMPARE_TYPES,
                       target_metadata=target_metadata,
                       process_revision_directives=process_revision_directives,
                       **current_app.extensions['migrate'].configure_args)
